2fdf4190f591d9f422e15ca6a779ce0dc8bf49f9
AherreDev <aherredev@gmail.com>
2020-03-20 19:43:33 -0600

    [Add] Clase 1

---
+++ clase1.js
@@ -0,0 +1,20 @@
+// ? Tipos de datos
+
+let a = 1; // * number
+let b = "String"; // * String
+let c = false; // * Boolean
+let d = []; // * Array <- Object
+let e = {}; // * Json <- Object
+let f = class popo {}; // * Clase <- Object
+// let FenElChat = new f();
+
+// console.log(`a = ${typeof a}`);
+// console.log(`b = ${typeof b}`);
+// console.log(`c = ${typeof c}`);
+// console.log(`d = ${typeof d}`);
+// console.log(`e = ${typeof e}`);
+// console.log(`f = ${typeof f}`);
+
+// ? Array
+
+// ? Ciclos
